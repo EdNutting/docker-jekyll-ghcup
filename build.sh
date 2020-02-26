30e59246599cfea73cb8f7922929b97260ebd23f
Ed Nutting <edwardnutting@outlook.com>
2020-02-26 15:58:21 +0000

    Simplify build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Note: `workspaceFolder` is the folder that contains the Dockerfile
-#       The name is inherited from VSCode meaning.
-#       See vscode-user-settings.json.example
-
-docker build -t ednutting/jekyll-ghcup:dev $workspaceFolder
+docker build -t ednutting/jekyll-ghcup:dev .
